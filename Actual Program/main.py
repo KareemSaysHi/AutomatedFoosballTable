e8122e5353db118f229d76d1f993aa29ce29a39a
Kareem Jaber <45048520+KareemSaysHi@users.noreply.github.com>
2022-05-04 12:46:52 -0400

    all code "should" be done

--- Actual Program/main.py
+++ Actual Program/main.py
@@ -1,3 +1,4 @@
+from argon2 import PasswordHasher
 import cvmethods
 import cv2
 import time
@@ -7,18 +8,20 @@ import numpy as np
 from centroidtracker import CentroidTracker
 from onnxhelper import OnnxHelper
 import serial
+import keyboard
 
 class AutomatedFoosballTable():
     
     def __init__(self):
-
+        
+        self.deltaTime = 0.05
         #open serial
         #self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5) 
         self.specialNumber = 123456789
 
         #define some standard values:
         self.isPlaying = False
-        self.rodPos = [0, 0, 0, 0, 0, 0]
+        self.rodPos = [0, 0, 0, 0, 0, 0] #enemy, offense, enemy, mid, enemy, defense
         self.rodRot = [None, 0, None, 0, None, 0]
         self.ballPos = 0
         self.noBallCounter = 0
@@ -181,7 +184,189 @@ class AutomatedFoosballTable():
                 key = cv2.waitKey(1) & 0xFF
                 if key == ord("q"):
                     break
-    
+
+    def mainThrees(self):
+        print("entering main but for threes")
+        while True:
+            self.totalStepCounter += 1 #update counters
+
+            frame = self.newFrame() #make a new frame
+
+            center = cvmethods.getBallPos(frame)[1] #get ball data
+            if center == None:  #if it doesn't see the ball, power off
+                self.noBallCounter += 1 #ball pos doesn't update if ball not found
+                if self.noBallCounter > 200:
+                    self.isPlaying = False
+            else: 
+                self.ballPos = center
+                self.noBallCounter = 0
+                self.isPlaying = True
+
+            if self.isPlaying: #everything else is contained in this if statement
+                
+                rodPoints = cvmethods.getRodPoints(frame)[0] #get rod data
+
+                objects = self.ct.update(rodPoints) #and update
+
+                self.rodPos = []
+                for i in range(0, len(objects.items())): #each item is (objectId, centroid)
+                    print(objects.items())
+                    self.rodPos.append([list(objects.items())[i][0]/frame.shape[1]]) #0 for the x value, normalize it
+
+                for (objectId, centroid) in objects.items(): #just for visualization
+                    text = "ID {}".format(objectId)
+                    cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                linoffSteps, linmidSteps, lindefSteps, rotoffSteps, rotmidSteps, rotdefSteps = self.oh.runOnnxThrees(self.ballPos, self.rodPos, self.rodRot) #run onnx
+
+                sendStr = str(linoffSteps) + "," + str(rotoffSteps) + "," + str(linmidSteps) + "," + str(rotmidSteps) + "," + str(lindefSteps) + "," + str(rotdefSteps) + "," + "\n" #format
+                sendStr = bytes(sendStr, 'utf-8')
+                self.ser.write(sendStr) #send to arduino
+
+                if self.totalStepCounter % self.readRotCounterLim == 0: #check if we need to update rotation values
+                    if self.ser.in_waiting: #see if serial has stuff in it
+                        serLine = int(self.ser.readline().strip('\n')) #format the line
+                        if serLine == self.specialNumber: #see if we have terminating number
+                            rodMaskArray = [1, 3, 5] 
+                            for i in range(0, 3):
+                                serLine = int(self.ser.readline().strip('\n'))
+                                self.rodRot[rodMaskArray[i]] = self.oh.stepsToRot(serLine) #update the changes
+
+                cv2.imshow("frame", frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord("q"):
+                    break
+
+    def keyboardControl(self):
+        while True:
+            self.totalStepCounter += 1 #update counters
+            frame = self.newFrame() #make a new frame
+            ball, center = cvmethods.getBallPos(frame) #get ball data
+            if center == None:  #if it doesn't see the ball, power off
+                self.noBallCounter += 1 #ball pos doesn't update if ball not found
+                if self.noBallCounter > 200:
+                    self.isPlaying = False
+            else: 
+                self.ballPos = center
+                self.noBallCounter = 0
+                self.isPlaying = True
+
+            if self.isPlaying: #everything else is contained in this if statement
+                
+                rodPoints = cvmethods.getRodPoints(frame)[0] #get rod data
+
+                objects = self.ct.update(rodPoints) #and update
+
+                self.rodPos = []
+                for i in range(0, len(objects.items())): #each item is (objectId, centroid)
+                    print(objects.items())
+                    self.rodPos.append([list(objects.items())[i][0]/frame.shape[1]]) #0 for the x value, normalize it
+
+
+                #essentially a heuristic
+                onnxMatrix = [1, 1]
+                if keyboard.read_key() == "q":
+                    onnxMatrix[0] = 0
+                elif keyboard.read_key() == "w":
+                    onnxMatrix[0] = 2
+                if keyboard.read_key() == "e":
+                    onnxMatrix[1] = 0
+                elif keyboard.read_key() == "r":
+                    onnxMatrix[1] = 2
+
+                linVel, rotVel = self.oh.rawOutputToVel(self.rodPos, self.rodRot, onnxMatrix) #run onnx on manufactured output
+                linSteps, rotSteps = self.oh.velToSteps(linVel, rotVel)
+                sendStr = str(linSteps) + "," + str(rotSteps) + ",0,0,0,0,\n" #format
+                sendStr = bytes(sendStr, 'utf-8')
+                self.ser.write(sendStr) #send to arduino
+
+                if self.totalStepCounter % self.readRotCounterLim == 0: #check if we need to update rotation values
+                    if self.ser.in_waiting: #see if serial has stuff in it
+                        serLine = int(self.ser.readline().strip('\n')) #format the line
+                        if serLine == self.specialNumber: #see if we have terminating number
+                            rodMaskArray = [1, 3, 5] 
+                            for i in range(0, 3):
+                                serLine = int(self.ser.readline().strip('\n'))
+                                self.rodRot[rodMaskArray[i]] = self.oh.stepsToRot(serLine) #update the changes
+
+                cv2.imshow("frame", frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord("q"):
+                    break
+
+    def keyboardControlThrees(self):
+        while True:
+            self.totalStepCounter += 1 #update counters
+            frame = self.newFrame() #make a new frame
+            ball, center = cvmethods.getBallPos(frame) #get ball data
+            if center == None:  #if it doesn't see the ball, power off
+                self.noBallCounter += 1 #ball pos doesn't update if ball not found
+                if self.noBallCounter > 200:
+                    self.isPlaying = False
+            else: 
+                self.ballPos = center
+                self.noBallCounter = 0
+                self.isPlaying = True
+
+            if self.isPlaying: #everything else is contained in this if statement
+                
+                rodPoints = cvmethods.getRodPoints(frame)[0] #get rod data
+
+                objects = self.ct.update(rodPoints) #and update
+
+                self.rodPos = []
+                for i in range(0, len(objects.items())): #each item is (objectId, centroid)
+                    print(objects.items())
+                    self.rodPos.append([list(objects.items())[i][0]/frame.shape[1]]) #0 for the x value, normalize it
+
+
+                #essentially a heuristic
+                onnxMatrix = [1, 1, 1, 1, 1, 1]
+                if keyboard.read_key() == "q":
+                    onnxMatrix[0] = 0
+                elif keyboard.read_key() == "w":
+                    onnxMatrix[0] = 2
+                if keyboard.read_key() == "e":
+                    onnxMatrix[3] = 0
+                elif keyboard.read_key() == "r":
+                    onnxMatrix[3] = 2
+                if keyboard.read_key() == "a":
+                    onnxMatrix[1] = 0
+                elif keyboard.read_key() == "s":
+                    onnxMatrix[1] = 2
+                if keyboard.read_key() == "d":
+                    onnxMatrix[4] = 0
+                elif keyboard.read_key() == "f":
+                    onnxMatrix[4] = 2
+                if keyboard.read_key() == "z":
+                    onnxMatrix[2] = 0
+                elif keyboard.read_key() == "x":
+                    onnxMatrix[2] = 2
+                if keyboard.read_key() == "c":
+                    onnxMatrix[5] = 0
+                elif keyboard.read_key() == "v":
+                    onnxMatrix[5] = 2
+
+                linoffSteps, linmidSteps, lindefSteps, rotoffSteps, rotmidSteps, rotdefSteps = self.oh.runOnnxThrees(self.ballPos, self.rodPos, self.rodRot) #run onnx
+
+                sendStr = str(linoffSteps) + "," + str(rotoffSteps) + "," + str(linmidSteps) + "," + str(rotmidSteps) + "," + str(lindefSteps) + "," + str(rotdefSteps) + "," + "\n" #format
+                sendStr = bytes(sendStr, 'utf-8')
+                self.ser.write(sendStr) #send to arduino
+
+                if self.totalStepCounter % self.readRotCounterLim == 0: #check if we need to update rotation values
+                    if self.ser.in_waiting: #see if serial has stuff in it
+                        serLine = int(self.ser.readline().strip('\n')) #format the line
+                        if serLine == self.specialNumber: #see if we have terminating number
+                            rodMaskArray = [1, 3, 5] 
+                            for i in range(0, 3):
+                                serLine = int(self.ser.readline().strip('\n'))
+                                self.rodRot[rodMaskArray[i]] = self.oh.stepsToRot(serLine) #update the changes
+
+                cv2.imshow("frame", frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord("q"):
+                    break
+        
     def newFrame(self):
         ret, frame = self.cap.read()
         frame = self.resize(frame, 50)
--- Actual Program/onnxhelper.py
+++ Actual Program/onnxhelper.py
@@ -1,3 +1,4 @@
+from pydoc import Helper
 import numpy as np
 import onnx
 import onnxruntime as ort
@@ -16,11 +17,32 @@ class OnnxHelper():
         self.lastInput = 0
         self.lastInput = 0
 
+        onnx_model = onnx.load(self.model_path)
+        onnx.checker.check_model(onnx_model)
+
         self.linearInput = 0
         self.rotaryInput = 0
 
-        onnx_model = onnx.load(self.model_path)
-        onnx.checker.check_model(onnx_model)
+        #three player variables
+
+        self.linearInputs = [0, 0, 0]
+        self.rotaryInputs = [0, 0, 0]
+
+        self.linearVelocityOffense = 0
+        self.linearVelocityMid = 0 
+        self.linearVelocityDefense = 0
+
+        self.linearVelocities = [self.linearVelocityOffense, self.linearVelocityMid, 
+        self.linearVelocityDefense]
+
+        self.angularVelocityOffense = 0
+        self.angularVelocityMid = 0
+        self.angularVelocityDefense = 0
+
+        self.angularVelocities = [self.angularVelocityOffense, self.angularVelocityMid, 
+        self.angularVelocityDefense]
+
+        self.maxPosArray = [3.6125, 2.5, 3.6125]
 
     def correctRodPos(self, rodPosArray):
         pass #this will adjust for the dist of where things are placed
@@ -45,6 +67,27 @@ class OnnxHelper():
         #agent.position (1)
         #agent.rotation (1)
         #enemyagent.position (1)
+
+    def setInputsThrees(self, ballPos, rodPosArray, rodRotArray):
+        self.lastInput = np.array([*toBallNormalized, toBallMagnitude, agentPos, agentRot, enemyPos], dtype=np.float32)
+
+        toBall = np.array([(ballPos[1] - rodPosArray[1][1])*self.unityZScalar, 0, (ballPos[0] - rodPosArray[1][0]*self.unityZScalar)])
+        #A few things to note: rodPosArray[1] refers to 2nd closest to top of frame (id 1)
+        #Unity's system is [our height, 0, our width] or [y, 0, x]
+        toBallMagnitude = np.linalg.norm(toBall) 
+        toBallNormalized = toBall/toBallMagnitude
+        
+        agentPos = rodPosArray[1]*self.unityZScalar
+        agentRot = rodRotArray[1]
+
+        enemyPos = rodPosArray[0]*self.unityZScalar        
+    
+        return np.array([*toBallNormalized, toBallMagnitude, rodPosArray[1][1], rodPosArray[3][1], rodPosArray[5][1], rodPosArray[1], rodPosArray[3], rodPosArray[5], rodPosArray[0][1], rodPosArray[2][1], rodPosArray[4][1]], dtype=np.float32)
+        #to ball.normalized (3)
+        #to ball.magnitude (1)
+        #agent.position (1)
+        #agent.rotation (1)
+        #enemyagent.position (1)
     
     def runOnnx(self, ballPos, rodPosArray, rodRotArray):
 
@@ -64,6 +107,34 @@ class OnnxHelper():
         linSteps, rotSteps = self.velToSteps(linVel, rotVel)
         
         return linSteps, rotSteps
+
+    def runOnnxThrees(self, ballPos, rodPosArray, rodRotArray, heuristic = False, manufacturedOutput = []):
+
+        if not heuristic:
+            
+            self.input = self.setInputsThrees(ballPos, rodPosArray, rodRotArray)
+
+            totalInputs = np.concatenate(self.input, self.lastInput, axis=0)
+            
+
+            #print([input.name for input in onnx_model.graph.input])
+            #print([output.name for output in onnx_model.graph.output])
+            ort_sess = ort.InferenceSession("Foosball.onnx")
+
+            outputs = ort_sess.run(["continuous_actions"], {'obs_0': totalInputs}) #None returns all of the outputs
+        
+        if heuristic:
+            outputs = manufacturedOutput
+
+        linoff, linmid, lindef, rotoff, rotmid, rotdef = self.rawOutputToVelThrees(self, rodPosArray, rodRotArray, outputs)
+
+        linoffSteps, rotoffSteps = self.velToSteps(linoff, rotoff)
+
+        linmidSteps, rotmidSteps = self.velToSteps(linmid, rotmid)
+
+        lindefSteps, rotdefSteps = self.velToSteps(lindef, rotdef)
+        
+        return linoffSteps, linmidSteps, lindefSteps, rotoffSteps, rotmidSteps, rotdefSteps
 
     def rawOutputToVel(self, rodPosArray, rodRotArray, outputArray): #currently only for one rod
         if (outputArray[0] == 0):
@@ -131,6 +202,79 @@ class OnnxHelper():
             self.rotaryInput = 0
 
         return self.foosLinearVelocity, self.foosAngularVelocity
+
+    def rawOutputToVelThrees(self, rodPosArray, rodRotArray, outputArray): #this one works for three
+
+        players = [1, 3, 5]
+        
+        for i in range (0, 3):
+            if outputArray[i] == 0:
+                self.linearInputs[i] -= 1
+            elif outputArray[i] == 2:
+                self.linearInputs[i] += 1
+
+        for i in range (3, 6):
+            if outputArray[i] == 0:
+                self.rotaryInputs[i-3] -= 1
+            elif outputArray[i] == 2:
+                self.rotaryInputs[i-3] += 1
+                
+        for i in range (0, 3):
+            if (self.linearInputs[i] >= 3):
+                self.linearInputs[i] = 3
+                self.linearVelocities[i] = 30
+            elif (self.linearInput == 2):
+                self.linearVelocities[i] = 8
+            elif (self.linearInput == 1):
+                self.linearVelocities[i] = 2
+            elif (self.linearInput == 0):
+                self.linearVelocities[i] = 0
+            elif (self.linearInput == -1):
+                self.linearVelocities[i] = -2
+            elif (self.linearInput == -2):
+                self.linearVelocities[i] = -8
+            else:
+                self.linearInputs[i] = -2
+                self.linearVelocities[i] = -30
+
+            newRodPosArray = rodPosArray #copy over
+            newRodPosArray[players[i]] = rodPosArray[players[i]] + self.linearVelocities[i] * self.fixedDeltaTime #update
+            if newRodPosArray[players[i]] > self.maxPosArray[i]: #if outside
+                newRodPosArray[players[i]] = self.maxPosition #fix
+                self.linearVelocities[i] = 0
+                self.linearInputs[i] = 0
+            elif newRodPosArray[players[i]] < -1*self.maxPosArray[i]:
+                newRodPosArray[players[i]] = -1*self.maxPosArray[i]
+                self.linearVelocities[i] = 0
+                self.linearInputs[i] = 0
+
+        for j in range (0, 3):
+            if (self.rotaryInputs[j] >= 2):
+                self.rotaryInputs[j] = 2
+                self.angularVelocities[j] = 450
+            elif (self.rotaryInputs[j] == 1):
+                self.angularVelocities[j] = 90
+            elif (self.rotaryInputs[j] == 0):
+                self.angularVelocities[j] = 0
+            elif (self.rotaryInputs[j] == -1):
+                self.angularVelocities[j] = -90
+            else:
+                self.rotaryInputs[j] = -2
+                self.angularVelocities[j] = -450        
+
+            newRodRotArray = rodRotArray #copy
+            newRodRotArray[players[j]] = rodRotArray[players[j]] + self.angularVelocities[j] * self.fixedDeltaTime #update
+            if newRodRotArray[players[j]] > self.maxRotation and newRodRotArray[players[j]] < 180:
+                newRodRotArray[players[j]] = self.maxRotation
+                self.angularVelocities[j] = 0
+                self.rotaryInputs[j] = 0
+            elif newRodRotArray[players[j]] < 360-1*self.maxRotation:
+                newRodRotArray[1] = 360-1*self.maxRotation #think about this more later
+                self.angularVelocities[j] = 0
+                self.rotaryInputs[j] = 0
+
+        return [*self.linearVelocites, *self.angularVelocities]
+
 
     def velToSteps(self, lin, rot):
         #to change speed to steps:
